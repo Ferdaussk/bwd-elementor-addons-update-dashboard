a1097a85dc38367b33daabc51c07bc6041282a9d
Ferdaussk <ferdauskhansk4925@gmail.com>
2024-05-25 18:15:31 +0600

    M

--- includes/tabs/license.php
+++ includes/tabs/license.php
@@ -9,7 +9,7 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 echo '<div id="license" class="bwd-admin-setting-tab">';
 		echo '<div class="bwd-license-container">';
 			echo '<div class="bwd-license-content">';
-				echo '<h3>'.esc_html__('Free vs Pro Table','bwd-elementor-addons').'</h3>';
+				echo '<h3>'.esc_html__('Compare with Free vs Pro','bwd-elementor-addons').'</h3>';
 				echo '<span>'.esc_html__('You can explore the feature that we provied with fun and have their own functions each feature.','bwd-elementor-addons').'</span>';
 			echo '</div>';
 			echo '<div class="bwd-license-table-area">';
